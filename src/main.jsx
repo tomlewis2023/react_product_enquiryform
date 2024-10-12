657e192f34dfeba920c4d579c4883bfe9c602f9b
tomlewis2023 <tomlewis2005@gmail.com>
2024-10-12 20:27:00 +0530

    second

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { HashRouter, Routes, Route } from "react-router-dom"; // Import HashRouter
-import Errorpage from './Errorpage.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./Errorpage.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ProductInquiry, { inquiryAction } from "./assets/components/Contact.jsx";
+import { inquiryAction } from "./assets/components/Contact.jsx";
 import Root from "./assets/components/Routes/Root.jsx";
 import Home from "./assets/components/Home.jsx";
+import ProductInquiry from "./assets/components/Contact.jsx";
 
- // Ensure this imports your ProductInquiry component
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
 
-const App = () => {
-  return (
-    <HashRouter>
-      <Root />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<ProductInquiry />} action={inquiryAction} />
-        {/* Add more routes as needed */}
-      </Routes>
-      <Route path="*" element={<Errorpage/>} /> {/* Catch-all route for 404 errors */}
-      {/* Error page should be handled as needed */}
-    </HashRouter>
-  );
-};
+      {
+        path: "/contact",
+        element: <ProductInquiry />,
+        action: inquiryAction,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
