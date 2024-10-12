46ba3b0fbfa8de194bb8a5e1154b3cc412c106a2
tomlewis2023 <tomlewis2005@gmail.com>
2024-10-12 20:43:49 +0530

    third

--- src/Errorpage.jsx
+++ src/Errorpage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+// ErrorPage.jsx
+import React from 'react';
 
-const Errorpage = () => {
+const ErrorPage = () => {
   return (
-    <div>Errorpage</div>
-  )
-}
+    <div>
+      <h1>Error 404</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
 
-export default Errorpage
+export default ErrorPage;
